ae5fcd3ae71602187af84f96b2acd98333bba217
Brandonacz1994 <38328809+Brandonacz1994@users.noreply.github.com>
2023-02-08 20:10:20 -0700

    added tema 1: figuras

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 import Intro from "./Scenes/test"
+import Tema_1_figuras from "./Scenes/Tema_1_Figuras"
 
 function App() {
   return (
     <Router>
       <Routes>
           <Route exact path='/' element={<Intro/>}></Route>
+          <Route path='/tema_1' element={<Tema_1_figuras/>}></Route>
       </Routes>
     </Router>
 )}
