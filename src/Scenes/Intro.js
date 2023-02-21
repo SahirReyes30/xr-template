386b4b4324b4d8544355d12eb2563d1f6abacaf5
Brandonacz1994 <38328809+Brandonacz1994@users.noreply.github.com>
2023-02-21 08:53:57 -0700

    added ammo.js

--- src/Scenes/Intro.js
+++ src/Scenes/Intro.js
@@ -10,31 +10,41 @@ import * as Materials_Module from "../Modules/Materials_Module";
 
 import * as Babylon_Components from "../Babylon_components"
 
+import ammo from "ammo.js"
 
-const onSceneReady = (e = { engine: new BABYLON.Engine, scene: new BABYLON.Scene, canvas: new HTMLCanvasElement }) => {
+
+const onSceneReady = async (e = { engine: new BABYLON.Engine, scene: new BABYLON.Scene, canvas: new HTMLCanvasElement }) => {
 
   const { canvas, scene, engine } = e;
-
 
   Cameras_Module.FreeCameraDefault(canvas, scene);
 
   Lights_Module.HemisphericLight(scene);
 
-  Babylon_Components.PlayGround();
+  var playground = Babylon_Components.PlayGround();
+
+  scene.enablePhysics(new BABYLON.Vector3(0,-9.81,0),new BABYLON.AmmoJSPlugin(true,await ammo()));
+
+
+  var ground_impostor = new BABYLON.PhysicsImpostor(playground.ground,BABYLON.PhysicsImpostor.BoxImpostor, {mass:0,restitution:0.9},scene);
+
+
+
+  var sphere = BABYLON.MeshBuilder.CreateSphere("sphere",{diameter: 3},scene);
+  sphere.position.y = 4;
+  var sphere_impostor = new BABYLON.PhysicsImpostor(sphere,BABYLON.PhysicsImpostor.SphereImpostor, {mass:1,restitution:0.9},scene);
+
+  
 
   var box_material = Materials_Module.MaterialRandom();
-
-
-
   // Our built-in 'box' shape.
   var box = BABYLON.MeshBuilder.CreateBox("box", { size: 2 }, scene);
   box.material =box_material;
-
   // Move the box upward 1/2 its height
   box.position = new BABYLON.Vector3(1,1,1);
+  var box_impostor = new BABYLON.PhysicsImpostor(box,BABYLON.PhysicsImpostor.BoxImpostor,{mass:1,restitution:0.9},scene);
 
-  Babylon_Components.showWorldAxis(10,scene);
- 
+  Babylon_Components.showWorldAxis(10,scene); 
   var localAxe1 = Babylon_Components.showLocalAxes(3,scene);
 
   localAxe1.parent = box;
@@ -44,7 +54,8 @@ const onSceneReady = (e = { engine: new BABYLON.Engine, scene: new BABYLON.Scene
       const deltaTimeInMillis = scene.getEngine().getDeltaTime();
 
       const rpm = 30
-      box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
+      //box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
+      box.rotate(BABYLON.Axis.Y,(rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000),scene);
     }
 
   });
